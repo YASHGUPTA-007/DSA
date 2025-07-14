289c80e05aba385a68677c9e7d0794c6528b9ee4
Yash Gupta <yash.gupta.11.7.2004@gmail.com>
2025-07-14 07:47:25 +0530

    Transpose of Matrix

--- 2d-Array/practice-1.cpp
+++ 2d-Array/practice-1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int TransposeOfMatrix(int arr,int n,int m){
-    cout << "Transpose of Array";
-    
-        for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << arr[j][i];
-        }
-        cout << endl;
-    }
+// int TransposeOfMatrix(int arr,int n,int m){
+//     cout << "Transpose of Array";
 
-}
+//         for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             cout << arr[j][i];
+//         }
+//         cout << endl;
+//     }
+
+// }
 
 int main()
 {
@@ -30,6 +30,8 @@ int main()
         cout << endl;
     }
     cout << "Printing Elements";
+    cout << endl;
+
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
@@ -38,8 +40,45 @@ int main()
         }
         cout << endl;
     }
+    cout << "Transpose of Array";
+    cout << endl;
 
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[j][i];
+        }
+        cout << endl;
+    }
 
-    TransposeOfMatrix(arr,n,m);
+    cout << "Transpose in the same Array";
+
+    cout << endl;
+
+    // Transpose in the same matrix
+    for (int i = 0; i < m; i++)
+    { // rows
+        for (int j = i + 1; j < n; j++)
+        { // cols
+            // Swapping arr[i][j] and arr[j][i]
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+        cout << endl;
+    }
+
+    // Print the in-place transposed matrix
+    cout << "\nMatrix after in-place transpose:" << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
